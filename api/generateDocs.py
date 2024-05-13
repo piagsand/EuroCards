2f7c98d3cd4eead332ad1a1951a0ed83550f404d
Sandy Piaget <spiaget@github.com>
2024-05-13 16:28:52 +0200

    generate report

--- api/generateDocs.py
+++ api/generateDocs.py
@@ -4,20 +4,17 @@ import io
 import json
 
 class handler(BaseHTTPRequestHandler):
-    def do_OPTIONS(self):
-        # Pré-gérer la requête OPTIONS pour les requêtes CORS
-        self.send_response(204)  # No Content
+    def send_cors_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+
+    def do_OPTIONS(self):
+        self.send_response(204)  # No Content
+        self.send_cors_headers()
         self.end_headers()
 
     def do_POST(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_response(200)
-        self.send_header('Content-type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-        self.end_headers()
-
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode('utf-8'))
@@ -26,9 +23,9 @@ class handler(BaseHTTPRequestHandler):
         doc.add_heading('Liste des Cartes', level=1)
 
         sections = [
-            ('Cartes manquantes', data['missingCards']),
-            ('Cartes acquises', data['ownedCards']),
-            ('Cartes doubles', data['doubleCards'])
+            ('Cartes manquantes', data.get('missingCards', [])),
+            ('Cartes acquises', data.get('ownedCards', [])),
+            ('Cartes doubles', data.get('duplicateCards', []))
         ]
 
         for title, cards in sections:
@@ -36,9 +33,19 @@ class handler(BaseHTTPRequestHandler):
             for card in cards:
                 doc.add_paragraph(card)
 
-        # Sauvegarde du document en mémoire
         mem_file = io.BytesIO()
         doc.save(mem_file)
         mem_file.seek(0)
 
+        self.send_response(200)
+        self.send_cors_headers()
+        self.send_header('Content-type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+        self.end_headers()
         self.wfile.write(mem_file.getvalue())
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_cors_headers()
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"API is working")
